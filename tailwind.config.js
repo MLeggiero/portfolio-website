c8efed0920d2e9c7b23e6ff0cb2e1f2c1bcd5c4f
MLeggiero <mark.leggiero1@gmail.com>
2026-01-02 16:04:11 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
             colors: {
                 background: '#0F172A', // Slate 900
                 surface: '#1E293B',    // Slate 800
-                primary: '#EF4444',    // Red 500
+                primary: '#243eb1ff',    // Red 500
                 text: '#F1F5F9',       // Slate 100
                 'text-muted': '#94A3B8', // Slate 400
             },
